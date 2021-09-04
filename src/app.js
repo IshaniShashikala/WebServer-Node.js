d96c9863cf8496ab13665d5a7a57d0232752a210
Ishani <ishaniist@gmail.com>
2021-09-04 19:24:42 +0530

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
@@ -7,10 +8,41 @@ const express = require('express')
 // console.log(__filename)
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//Define paths for Express config 
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views') //customize views foldeer and path
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directoy to serve
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res)=>{
+    res.render('index',{
+        title:'Weather',
+        name: 'Ishani'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about',{
+        title:'About Me',
+        name: 'Ishani'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help',{
+        helpText:'This is some helpful text',
+        title:'Help',
+        name: 'Ishani'
+    })
+})
 // app.get('',(req, res)=>{
 //     res.send('<h1>Weather</h1>')
 // })
@@ -38,6 +70,23 @@ app.get('/weather', (req,res)=>{
         location: 'Dublin'
     })
 })
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Ishani',
+        errorMessage:'Help aticle not found'
+    })
+   })
+
+app.get('*',(req,res)=>{
+ res.render('404',{
+     title:'404',
+     name:'Ishani',
+     errorMessage:'Page not found'
+ })
+})
+
 
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000')
